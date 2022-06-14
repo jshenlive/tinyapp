0808761d72851ce57ea035385ed7d4dd31441822
Jesse Shen <jly.shen@gmail.com>
2022-06-14 20:39:38 +0000

    new url and nodemon

--- express_server.js
+++ express_server.js
@@ -2,6 +2,9 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+
 //setting ejs as template engine for Express.app
 app.set("view engine", "ejs");
 
@@ -28,12 +31,39 @@ app.get("/urls", (req, res) => {
   res.render("urls_index",templateVars);
 });
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: req.params.longURL };
   res.render("urls_show", templateVars);
 });
 
+app.get("/u/:shortURL", (req, res) => {
+  const longURL = urlDatabase[req.params.shortURL]
+  res.redirect(longURL);
+});
+
+app.post("/urls", (req, res) => {  
+  // console.log(req.body);  // Log the POST request body to the console
+  let shortUrl = generateRandomString();
+  urlDatabase[shortUrl] = req.body.longURL;
+  // console.log(urlDatabase);
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+});
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
+
+function generateRandomString(){
+  const len = 6;
+  const values = [0,1,2,3,4,5,6,7,8,9,'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+
+  let result = '';
+  for (let i = 0; i<len;i++){
+    result += values[Math.floor(Math.random()*values.length)];
+  }
+  return result;
+}
